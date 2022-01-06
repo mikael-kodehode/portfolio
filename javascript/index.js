8e89190954858f80628c4f756c6787a43e637d3a
Mikael Ødegaard <93524362+mikael-kodehode@users.noreply.github.com>
2022-01-06 18:52:30 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,50 @@
+const homeBtn = document.getElementById("home")
+const projectsBtn = document.getElementById("projects")
+const cvBtn = document.getElementById("cv")
 
-const langList = document.getElementById("lang-list");
-const heroBtn = document.getElementById("hero-btn");
+const homeEl = document.getElementById("home-content")
+const projectsEl = document.getElementById("projects-content")
+const cvEl = document.getElementById("cv-content")
 
-active = false;
+function removeActivePage() {
+    homeBtn.classList.remove("active-page")
+    projectsBtn.classList.remove("active-page")
+    cvBtn.classList.remove("active-page")
+}
+function removeContent() {
+    removeActivePage()
+    homeEl.classList.add("hidden")
+    projectsEl.classList.add("hidden")
+    cvEl.classList.add("hidden")
+}
+homeBtn.addEventListener("click", function() {
+    removeContent()
+    homeBtn.classList.add("active-page")
+    homeEl.classList.remove("hidden")
+})
+cvBtn.addEventListener("click", function() {
+    removeContent()
+    cvBtn.classList.add("active-page")
+    cvEl.classList.remove("hidden")
+})
 
 // This drops down the language list
-function showHeroList() {
-    langList.innerHTML = `<li>HTML/CSS/Javascript</li><li>Python</li>`
+
+const cube = document.querySelector('.cube');
+const radioGroup = document.querySelector('.radio-group');
+
+let currentClass = '';
+
+function changeSide() {
+  const checkedRadio = radioGroup.querySelector(':checked');
+  let showClass = 'show-' + checkedRadio.value;
+  if ( currentClass ) {
+    cube.classList.remove( currentClass );
+  }
+  cube.classList.add( showClass );
+  currentClass = showClass;
 }
-function hideHeroList() {
-    langList.innerHTML = ""
-}
-heroBtn.addEventListener("click", function() {
-    if (active === false) {
-        showHeroList();
-        active = true;
-    }
-    else if (active === true) {
-        hideHeroList();
-        active = false
-    }
-    else {
-        console.log("Error")
-    }
-})
+// set initial side
+changeSide();
+
+radioGroup.addEventListener( 'change', changeSide );
