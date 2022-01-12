51445fa4805c40a53488b611cbe339156495da3b
mikael-kodehode <93524362+mikael-kodehode@users.noreply.github.com>
2022-01-12 16:23:41 +0100

    Update
    General update

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,90 @@
-const homeBtn = document.getElementById("home")
+const homeBtn = document.querySelector("#home")
 const projectsBtn = document.getElementById("projects")
 const cvBtn = document.getElementById("cv")
+const firstPBtn = document.querySelector("#first-p")
+const secondPBtn = document.querySelector("#second-p")
+const thirdPBtn = document.querySelector("#third-p")
 
 const homeEl = document.getElementById("home-content")
 const projectsEl = document.getElementById("projects-content")
 const cvEl = document.getElementById("cv-content")
+const firstPEl = document.querySelector("#p1")
+const secondPEl = document.querySelector("#p2")
+const thirdPEl = document.querySelector("#p3")
 
 function removeActivePage() {
     homeBtn.classList.remove("active-page")
     projectsBtn.classList.remove("active-page")
     cvBtn.classList.remove("active-page")
 }
+
+function removeActiveParagraph() {
+    firstPBtn.classList.remove("active-p")
+    secondPBtn.classList.remove("active-p")
+    thirdPBtn.classList.remove("active-p")
+}
+
 function removeContent() {
     removeActivePage()
     homeEl.classList.add("hidden")
     projectsEl.classList.add("hidden")
     cvEl.classList.add("hidden")
 }
+
+function removeP() {
+    firstPEl.classList.add("hidden")
+    secondPEl.classList.add("hidden")
+    thirdPEl.classList.add("hidden")
+}
+
 homeBtn.addEventListener("click", function() {
     removeContent()
     homeBtn.classList.add("active-page")
     homeEl.classList.remove("hidden")
 })
+
+projectsBtn.addEventListener("click", function() {
+    removeContent()
+    projectsBtn.classList.add("active-page")
+    projectsEl.classList.remove("hidden")
+})
+
 cvBtn.addEventListener("click", function() {
     removeContent()
     cvBtn.classList.add("active-page")
     cvEl.classList.remove("hidden")
 })
 
-// This drops down the language list
+firstPBtn.addEventListener("click", function() {
+  removeActiveParagraph()
+  removeP()
+  firstPBtn.classList.add("active-p")
+  firstPEl.classList.remove("hidden")
+})
+
+secondPBtn.addEventListener("click", function() {
+  removeActiveParagraph()
+  removeP()
+  secondPBtn.classList.add("active-p")
+  secondPEl.classList.remove("hidden")
+})
+
+thirdPBtn.addEventListener("click", function() {
+  removeActiveParagraph()
+  removeP()
+  thirdPBtn.classList.add("active-p")
+  thirdPEl.classList.remove("hidden")
+})
+
+// Heartrate simulator
+const rate = document.querySelector("#rate");
+const heartContainer = document.querySelector("#heart-container")
+
+rate.addEventListener("input", function() {
+    let x = rate.value;
+    let y = (60 / x) 
+    heartContainer.style.animation = "container ease-out " + y + "s infinite";
+})
 
 const cube = document.querySelector('.cube');
 const radioGroup = document.querySelector('.radio-group');
@@ -38,13 +94,13 @@ let currentClass = '';
 function changeSide() {
   const checkedRadio = radioGroup.querySelector(':checked');
   let showClass = 'show-' + checkedRadio.value;
-  if ( currentClass ) {
-    cube.classList.remove( currentClass );
+  if (currentClass) {
+    cube.classList.remove(currentClass);
   }
-  cube.classList.add( showClass );
+  cube.classList.add(showClass);
   currentClass = showClass;
 }
 // set initial side
 changeSide();
 
-radioGroup.addEventListener( 'change', changeSide );
+radioGroup.addEventListener('change', changeSide);
