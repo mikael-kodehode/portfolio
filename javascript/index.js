a94e46f40191d39dc56751de49fd30689a325b6c
mikael-kodehode <93524362+mikael-kodehode@users.noreply.github.com>
2022-02-10 10:28:30 +0100

    Next is looping in company experiences

--- javascript/index.js
+++ javascript/index.js
@@ -9,7 +9,7 @@ const homePs = document.querySelectorAll(".p")
 const footerText = document.querySelector(".small-text")
 
 // Page projects
-const projectsContainer = document.querySelector(".col)
+const projectsContainer = document.querySelector(".col")
 
 // Page cv
 const cvIntro = document.querySelector("#cv-intro")
@@ -17,30 +17,72 @@ const contacts = document.querySelectorAll(".cv-ul")
 const cvNav = document.querySelectorAll(".cv-nav-li")
 // Experience
 const employersNav = document.querySelector("#employers")
+const employersBtns = document.querySelectorAll(".exp")
 const employer = document.querySelectorAll(".employer")
 // Education
 const education = document.querySelector("#education")
 // Courses/Volunteers
-const miscEl.document.querySelector("#misc-el")
+const miscEl = document.querySelector("#misc-el")
 
-async function getLanguage(language) {
-  const res = await fetch(`../javascript/${language}.json`)
+// Fetching data
+async function getLanguage(lang) {
+  const res = await fetch(`../javascript/${lang}.json`)
   const data = await res.json()
-  console.log(data)
   return data
 }
 
 function pushProject(data) {
   const project = document.createElement("p")
   project.classList.add("github-projects")
-  if (data.github === true) {
-    project.innerHTML = `<a href="${data.url}" target=_blank">${data.title}</a>`
-  }
+  if (data.github === true) { project.innerHTML = `<a href="${data.url}" target=_blank">${data.title}</a><img class="github-logo" src="images/github-logo.png" alt="github-logo">`}
+  else { project.innerHTML = `<a href="${data.url}" target=_blank">${data.title}</a>`}
   projectsContainer.append(project)
 }
 
+function pushLanguage(data) {
+    contacts[0].innerHTML = ""
+    contacts[1].innerHTML = ""
+  for (let x = 0; x < cvNav.length;x++) {
+    mainNav[x].textContent = data.nav[x]
+    cvNav[x].textContent = data.cv.nav[x]
+    homePs[x].textContent = data.paragraphs[x].description
+    contacts[0].innerHTML += `<li>${data.cv.contacts[x].title}</li>`
+    contacts[1].innerHTML += `<li>${data.cv.contacts[x].details}</li>`
+  }
 
+  for (let x = 0; x < cubeText.length; x++) {
+    cubeText[x].textContent = data.cube[x]
+  }
 
+  document.querySelector(".big-link").textContent = data.projectTitle
+  projectsContainer.innerHTML = ""
+  data.projects.forEach(pushProject)
+
+  cvIntro.innerHTML = `${data.cv.title}`
+  employersNav.innerHMTL = ""
+  for (x of data.cv.experience) {
+    employersNav.innerHTML += `<li class="p-nav-list exp>${x.navName}</li>`
+  }
+
+  footerText.innerHTML = `${data.footer[0]}<br>${data.footer[1]}`
+}
+
+function getData(data) {
+  switch (data) {
+    case "english":
+    getLanguage(data)
+    .then(pushLanguage)
+    .catch(e => console.log(e))
+    break
+    case "norwegian":
+    getLanguage(data)
+    .then(pushLanguage)
+    .catch(e => console.log(e))
+    break
+  }
+}
+buttons[0].addEventListener("click", () =>   {getData("english")})
+buttons[1].addEventListener("click", () => {getData("norwegian")})
 
 
 
